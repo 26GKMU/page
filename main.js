b8deafbef77f9c1aa412a04434625c30b1071432
26GKMU <26gitkmu@gmail.com>
2025-07-18 13:24:53 +0900

    toppage

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
-const TB = document.getElementById("textbox");
-const TC = document.getElementById("textcount");
-
-let x ;
-TB.addEventListener('keyup',(e) =>{
-    x=10-TB.value.length;
-    console.log(TB.value);
-    TC.textContent = `残り${x}文字`;
-    if(x>=0){
-        TC.style.color ="black";
-    }else{
-        TC.style.color ="red";
-    }
-});
+window.onload = () =>{
+  const url1 = 'https://tegaki-memo.onrender.com';
+  const data1 = UrlFetchApp.fetch(url1).getContentText();
+  const url2 = 'https://dentaku-ubn8.onrender.com';
+  var data2 = UrlFetchApp.fetch(url2).getContentText();
+}

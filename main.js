c45160480ec4a39db9ef9d80099da5ad91d4b565
26GKMU <26gitkmu@gmail.com>
2025-07-18 13:38:01 +0900

    renderウォームアップ

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-window.onload = () =>{
-  const url1 = 'https://tegaki-memo.onrender.com';
-  const data1 = UrlFetchApp.fetch(url1).getContentText();
-  const url2 = 'https://dentaku-ubn8.onrender.com';
-  var data2 = UrlFetchApp.fetch(url2).getContentText();
-}
+window.addEventListener('load', () => {
+  fetch('https://tegaki-memo.onrender.com/memos', { mode: 'no-cors' })
+  .then(res => console.log(res));
+  fetch('https://dentaku-ubn8.onrender.com/', { mode: 'no-cors' })
+  .then(res => console.log(res));
+});

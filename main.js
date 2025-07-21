584ccfe999e933df6aa29ec0a4216c21014bbcf6
26GKMU <26gitkmu@gmail.com>
2025-07-21 15:08:50 +0900

    layout

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const warmupServer = () =>{
-  fetch('https://tegaki-memo.onrender.com/memos', { mode: 'no-cors' })
-  fetch('https://dentaku-ubn8.onrender.com/', { mode: 'no-cors' })
+  //fetch('https://tegaki-memo.onrender.com/memos', { mode: 'no-cors' })
+  //fetch('https://dentaku-ubn8.onrender.com/', { mode: 'no-cors' })
+  console.log("test用のウォームアップ実行停止")
 }
 
 window.addEventListener('load', () => {
